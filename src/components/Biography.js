ab51408258e51d89bd42878b6089e4dad5558362
Roman <tkalichroman90@gmail.com>
2021-03-04 20:23:10 +0200

    add sortJS

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -2,38 +2,58 @@ import React, {Component} from 'react';
 import Event from './Event'
 
 
+const initialList =  [
+    {year: 1828, event: 'Родился в городе Нант'},
+    {year: 1847, event: 'Едет учиться в Париж на юриспрудента'},
+    {year: 1851, event: 'Впервые печатается в журнале "Musée des familles"'},
+    {year: 1860, event: 'Женится на Онорине де Виан'},
+    {year: 1861, event: 'Рождение сына Мишеля'},
+    {year: 1863, event: 'Выходит в большой тираж роман "Путешествие на воздушном шаре"'},
+    {year: 1865, event: 'Покупает яхту "Сен-мишель",которая становиться рабочим местом писателя'},
+    {year: 1905, event: 'Умирает от сахарного диабета'},
+]
+const date = 'Дата'
+const event = 'Событие'
+
 
  class Biography extends Component {
     
     state = {
-        eventList: [
-            {year: 1828, event: ' Родился в городе Нант'},
-            {year: 1847, event: ' Едет учиться в Париж на юриспрудента'},
-            {year: 1851, event: ' Впервые печатается в журнале "Musée des familles"'},
-            {year: 1860, event: ' Женится на Онорине де Виан'},
-            {year: 1861, event: ' Рождение сына Мишеля'},
-            {year: 1863, event: ' Выходит в большой тираж роман "Путешествие на воздушном шаре"'},
-            {year: 1865, event: ' Покупает яхту "Сен-мишель,которая становиться рабочим местом писателя'},
-            {year: 1905, event: ' Умирает от сахарного диабета'},
-        ],
-        listTitle: 'Биография Жюль Верна'
+        eventList: initialList,
+        listTitle: 'Биография Жюль Верна',
+        sortType: 'sortJS',
+        isSortDown: true
     }
+    
+    handleSort = (type) => {
+        
+        let sortedList = []
+        
+        // const firstEl = this.state.eventList[0][type]
+        // const lastEl = this.state.eventList[this.state.eventList.length - 1][type]
+            
+            sortedList = this.state.eventList.sort((a, b)=>{
+                if (a[type] > b[type]) {
+                    return this.state.isSortDown ? 1 : -1;
+                  }
+                  if (a[type] < b[type]) {
+                    return this.state.isSortDown ? -1 : 1;
+                  }
 
-    onSort = () => {
-        sort(function (a,b) {
-            return b.year - a.year
-        })
+                  return 0;
+
+            })
+        this.setState(prevValue => ({...prevValue, isSortDown:!prevValue.isSortDown, eventList: sortedList})) 
     }
     render (){
-        const date = 'Дата';
-        const event = 'Событие'
+        
         return (
             <div>
                 <h2>{this.state.listTitle}</h2>
                 <table id='biography'>
                 <tr>
-                    <td onClick={this.onSort.bind(null, date)}><strong>{date}</strong></td>
-                    <td onClick={this.onSort.bind(null, event)}> <strong>{event}</strong></td>
+                    <td onClick={() => this.handleSort('year')}><strong>{date}</strong></td>
+                    <td onClick={() => this.handleSort('event')} > <strong>{event}</strong></td>
                 </tr>
                 {this.state.eventList.map((year, index)=>{
                     return (
@@ -41,7 +61,7 @@ import Event from './Event'
                         key={index}
                         year={year.year}
                         event={year.event}
-                        onSort={this.onSort}
+                        onSort={this.setState.onSort}
                         />
                     )
                 })}
